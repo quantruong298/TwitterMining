2acdad9e65f27ad7129717ea2d28c9e795bfb40d
Minhquan <kubim298@gmail.com>
2019-10-27 15:35:25 +0700

    Remove email and newline characters

--- MLPlus-Example/example.py
+++ MLPlus-Example/example.py
@@ -1,4 +1,4 @@
-# import re
+import re
 # import numpy as np
 import pandas as pd
 # from pprint import pprint
@@ -25,13 +25,33 @@ import pandas as pd
 # import warnings
 # warnings.filterwarnings("ignore",category=DeprecationWarning)
 
-
+# ==========DONE STEP 3=====================
 # NLTK Stop words
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
+# ==========DONE STEP 5=====================
+
 # Import Dataset
 df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
-print(df.target_names.unique())
+# print(df.target_names.unique())
+df.target_names.unique()
 df.head()
+
+# ==========DONE STEP 6=====================
+# Convert to list
+data = df.content.values.tolist()
+
+# Remove Emails
+data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
+
+# Remove new line characters
+data = [re.sub('\s+', ' ', sent) for sent in data]
+
+# Remove distracting single quotes
+data = [re.sub("\'", "", sent) for sent in data]
+
+print(data[:1])
+
+# ==========DONE STEP 7=====================
