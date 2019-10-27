98ceab6681f76ad6775a2a384093dd7bb70af485
quan <kubim298@gmail.com>
2019-10-27 20:32:58 +0700

    View the topics in LDA model

--- MLPlus-Example/example.py
+++ MLPlus-Example/example.py
@@ -136,7 +136,7 @@ texts = data_lemmatized
 corpus = [id2word.doc2bow(text) for text in texts]
 
 # View
-print(corpus[:1])
+# print(corpus[:1])
 
 # # ==========DONE STEP 11=====================
 # Build LDA model
@@ -146,3 +146,9 @@ lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_
                                            passes=10,
                                            alpha='auto',
                                            per_word_topics=True)
+
+# # ==========DONE STEP 12====================
+
+# Print the Keyword in the 10 topics
+pprint(lda_model.print_topics())
+doc_lda = lda_model[corpus]
