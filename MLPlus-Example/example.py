be7339e1ec13505d07364ee8decb5401742513cb
quan <kubim298@gmail.com>
2019-10-27 19:40:38 +0700

    Create Bigram and Trigram Models

--- MLPlus-Example/example.py
+++ MLPlus-Example/example.py
@@ -57,19 +57,31 @@ data = [re.sub('\s+', ' ', sent) for sent in data]
 # Remove distracting single quotes
 data = [re.sub("\'", "", sent) for sent in data]
 
-print(data[:1])
+# print(data[:1])
 
 # # ==========DONE STEP 7=====================
-#
-#
-# def sent_to_words(sentences):
-#     for sentence in sentences:
-#         yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
-#         # deacc=True removes punctuations
-#
-#
-# data_words = list(sent_to_words(data))
-#
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
+        # deacc=True removes punctuations
+
+
+data_words = list(sent_to_words(data))
 # print(data_words[:1])
-#
+
 # # ==========DONE STEP 8=====================
+
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+
+# Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# See trigram example
+# print(trigram_mod[bigram_mod[data_words[0]]])
+
+# # ==========DONE STEP 9=====================
