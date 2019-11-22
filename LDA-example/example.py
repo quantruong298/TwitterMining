d5cfcde1b3b6d1b023048bd4d51fbec967ddbaf1
quan <kubim298@gmail.com>
2019-11-22 13:40:37 +0700

    use twitter data for dataset

--- LDA-example/example.py
+++ LDA-example/example.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import requests
+from requests_oauthlib import OAuth1
 import sys
 import pandas as pd
 import nltk
@@ -29,10 +31,30 @@ def preprocess(text):
     return result
 
 
-data = pd.read_csv('abcnews-date-text.csv', error_bad_lines=False)
-data_text = data[['headline_text']]
+screen_name = 'BillGates'
+url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + screen_name + '&tweet_mode=extended'
+
+
+# Data for Twitter Authentication
+client_key = 'dZFi155G5IDOhM47hxYJBmcjb'
+client_secret = 'O49HwYzLVPIPfp2nSVsPmhu7MUulE5x2bUKDjiMctt1JlqN4LG'
+resource_owner_key = '1178195198389583872-mHeRnLHYGxAprLgeBrMLn3CcT4ny7P'
+resource_owner_secret = 'BWpvwlOcafaLUjygWmaYquzXjBFGJ56AUImA7XQlUTG9L'
+
+oauth = OAuth1(client_key,
+               client_secret,
+               resource_owner_key,
+               resource_owner_secret,
+               signature_type='auth_header')
+response = requests.get(url, auth=oauth)
+tweets = response.json()
+data = pd.DataFrame(data=[tweet['full_text'] for tweet in tweets], columns=['tweet_text'])
+
+
+data_text = data[['tweet_text']]
 data_text['index'] = data_text.index
 documents = data_text
+
 
 # print(len(documents))
 # print(documents[:5])
@@ -46,11 +68,11 @@ documents = data_text
 # # print('\n\n tokenized and lemmatized document: ')
 # print(preprocess(doc_sample))
 
-processed_docs = documents['headline_text'].map(preprocess)
-print (processed_docs[:10])
+processed_docs = documents['tweet_text'].map(preprocess)
 
 
 dictionary = gensim.corpora.Dictionary(processed_docs)
+
 count = 0
 for k, v in dictionary.iteritems():
     print(k, v)
@@ -58,16 +80,17 @@ for k, v in dictionary.iteritems():
     if count > 10:
         break
 
-dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+
+# dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
 
 bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
 print (bow_corpus[10])
 
-bow_doc_4310 = bow_corpus[10]
-for i in range(len(bow_doc_4310)):
-    print("Word {} (\"{}\") appears {} time.".format(bow_doc_4310[i][0],
-                                           dictionary[bow_doc_4310[i][0]],
-                                                     bow_doc_4310[i][1]))
+# bow_doc_4310 = bow_corpus[10]
+# for i in range(len(bow_doc_4310)):
+#     print("Word {} (\"{}\") appears {} time.".format(bow_doc_4310[i][0],
+#                                            dictionary[bow_doc_4310[i][0]],
+#                                                      bow_doc_4310[i][1]))
 
 lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
 
@@ -75,8 +98,8 @@ for idx, topic in lda_model.print_topics(-1):
     print('Topic: {} \nWords: {}'.format(idx, topic))
 
 
-for index, score in sorted(lda_model[bow_corpus[4310]], key=lambda tup: -1*tup[1]):
-    print("\nScore: {}\t \nTopic: {}".format(score, lda_model.print_topic(index, 10)))
+# for index, score in sorted(lda_model[bow_corpus[4310]], key=lambda tup: -1*tup[1]):
+#     print("\nScore: {}\t \nTopic: {}".format(score, lda_model.print_topic(index, 10)))
 
 
 unseen_document = "Expanding the diversity of therapies in the pipeline and more efficiently recruiting patients for clinical trials will increase our odds of discovering a breakthrough. I’m hopeful it will lead to an intervention that reduces the impact of Alzheimer"
