b7e5be183364fa4c6351579fba82006b0e7ce219
quan <kubim298@gmail.com>
2019-11-23 00:20:41 +0700

    remove "https:~~"

--- UseTwitterAPI/GetAllTweet.py
+++ UseTwitterAPI/GetAllTweet.py
@@ -1,11 +1,11 @@
 import tweepy
 import sys
 import pandas
-
+import re
 # Setting for display full data in DataFrame
-# pandas.set_option('display.max_rows', None)
-# pandas.set_option('display.max_columns', None)
-# pandas.set_option('display.width', None)
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.width', None)
 pandas.set_option('display.max_colwidth', -1)
 
 # Consumer keys and access tokens, used for OAuth
@@ -21,7 +21,9 @@ auth.set_access_token(access_token, access_token_secret)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 tweet_list = pandas.DataFrame(columns=['tweet_text'])
-for status in tweepy.Cursor(api.user_timeline, screen_name='@RobertDowneyJr', count=200, tweet_mode="extended").items():
-    tweet_list = tweet_list.append({'tweet_text':status.full_text}, ignore_index=True)
+for status in tweepy.Cursor(api.user_timeline, screen_name='@BillGates', count=200, tweet_mode="extended").items():
+    tweet_list = tweet_list.append({'tweet_text':re.sub(r"http\S+", "", status.full_text)}, ignore_index=True)
 
 print (tweet_list)
+
+
