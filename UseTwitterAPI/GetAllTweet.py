74bd85b8bede0eeb6012ed442205cdf22d1ccc18
quan <kubim298@gmail.com>
2019-11-28 16:55:13 +0700

    build API server

--- UseTwitterAPI/GetAllTweet.py
+++ UseTwitterAPI/GetAllTweet.py
@@ -2,11 +2,14 @@ import tweepy
 import sys
 import pandas
 import re
+import json
+from flask_api import FlaskAPI
+from flask_cors import CORS
 # Setting for display full data in DataFrame
-pandas.set_option('display.max_rows', None)
-pandas.set_option('display.max_columns', None)
-pandas.set_option('display.width', None)
-pandas.set_option('display.max_colwidth', -1)
+# pandas.set_option('display.max_rows', None)
+# pandas.set_option('display.max_columns', None)
+# pandas.set_option('display.width', None)
+# pandas.set_option('display.max_colwidth', -1)
 
 # Consumer keys and access tokens, used for OAuth
 consumer_key = 'dZFi155G5IDOhM47hxYJBmcjb'
@@ -20,10 +23,24 @@ auth.set_access_token(access_token, access_token_secret)
 
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
-tweet_list = pandas.DataFrame(columns=['tweet_text'])
-for status in tweepy.Cursor(api.user_timeline, screen_name='@BillGates', count=200, tweet_mode="extended").items():
-    tweet_list = tweet_list.append({'tweet_text':re.sub(r"http\S+", "", status.full_text)}, ignore_index=True)
 
-print (tweet_list)
+app = FlaskAPI(__name__)
+CORS(app)
+
+tweet_list = pandas.DataFrame(columns=['created_at', 'tweet_text'])
 
 
+@app.route('/')
+def getTweets():
+    global tweet_list
+    for status in tweepy.Cursor(api.user_timeline, screen_name='@BillGates', count=200, tweet_mode="extended").items():
+        tweet_list = tweet_list.append({
+            'created_at': status.created_at,
+            'tweet_text': re.sub(r"http\S+", "", status.full_text)
+        }, ignore_index=True)
+
+    return tweet_list.to_json(orient='records')
+
+
+if __name__ == '__main__':
+    app.run()
