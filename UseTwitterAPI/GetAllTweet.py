dfe0548cb4e80a312b1f9d6b693e77c3ca7fb6cc
quan <kubim298@gmail.com>
2019-12-04 21:58:51 +0700

    add column "created_date"

--- UseTwitterAPI/GetAllTweet.py
+++ UseTwitterAPI/GetAllTweet.py
@@ -3,10 +3,10 @@ import sys
 import pandas
 import re
 # Setting for display full data in DataFrame
-pandas.set_option('display.max_rows', None)
-pandas.set_option('display.max_columns', None)
-pandas.set_option('display.width', None)
-pandas.set_option('display.max_colwidth', -1)
+# pandas.set_option('display.max_rows', None)
+# pandas.set_option('display.max_columns', None)
+# pandas.set_option('display.width', None)
+# pandas.set_option('display.max_colwidth', -1)
 
 # Consumer keys and access tokens, used for OAuth
 consumer_key = 'dZFi155G5IDOhM47hxYJBmcjb'
@@ -20,9 +20,12 @@ auth.set_access_token(access_token, access_token_secret)
 
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
-tweet_list = pandas.DataFrame(columns=['tweet_text'])
-for status in tweepy.Cursor(api.user_timeline, screen_name='@BillGates', count=200, tweet_mode="extended").items():
-    tweet_list = tweet_list.append({'tweet_text':re.sub(r"http\S+", "", status.full_text)}, ignore_index=True)
+tweet_list = pandas.DataFrame(columns=['created_date','tweet_text'])
+for status in tweepy.Cursor(api.user_timeline, screen_name='@RobertDowneyJr', count=200, tweet_mode="extended").items():
+    tweet_list = tweet_list.append({
+        'tweet_text': re.sub(r"http\S+", "", status.full_text),
+        'created_date': status.created_at
+    }, ignore_index=True)
 
 print (tweet_list)
 
