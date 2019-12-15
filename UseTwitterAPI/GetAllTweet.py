eccd77cb33b497dcc98b0de432017ca00bd90119
quan <kubim298@gmail.com>
2019-12-15 20:06:07 +0700

    API Server:simple_processing

--- UseTwitterAPI/GetAllTweet.py
+++ UseTwitterAPI/GetAllTweet.py
@@ -1,11 +1,15 @@
+from __future__ import unicode_literals
 import tweepy
 import sys
+import gensim
 import pandas
 import re
 import json
 from flask_api import FlaskAPI
 from flask_cors import CORS
 from datetime import datetime
+reload(sys)
+sys.setdefaultencoding('utf8')
 # Setting for display full data in DataFrame
 # pandas.set_option('display.max_rows', None)
 # pandas.set_option('display.max_columns', None)
@@ -13,6 +17,7 @@ from datetime import datetime
 # pandas.set_option('display.max_colwidth', -1)
 
 # Consumer keys and access tokens, used for OAuth
+
 consumer_key = 'dZFi155G5IDOhM47hxYJBmcjb'
 consumer_secret = 'O49HwYzLVPIPfp2nSVsPmhu7MUulE5x2bUKDjiMctt1JlqN4LG'
 access_token = '1178195198389583872-mHeRnLHYGxAprLgeBrMLn3CcT4ny7P'
@@ -31,6 +36,16 @@ CORS(app)
 tweet_list = pandas.DataFrame(columns=['created_at', 'tweet_text'])
 topics = ""
 
+# ======================= #
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield gensim.utils.simple_preprocess(str(sentence), deacc=True)  # deacc=True removes punctuations
+
+
+# ======================= #
+
 
 @app.route('/<screenName>')
 def getTweets(screenName):
@@ -41,13 +56,17 @@ def getTweets(screenName):
             'tweet_text': re.sub(r"http\S+", "", status.full_text)
         }, ignore_index=True)
 
-    return tweet_list.to_json(orient='records')
+    response = tweet_list.to_json(orient='records')
+    return response
 
 
 @app.route('/topics')
 def findTopics():
-    global topics
+    data = list(tweet_list['tweet_text'])
+    data_words = list(sent_to_words(data))
 
-    return topics
+    return data_words
+
+
 if __name__ == '__main__':
     app.run()
