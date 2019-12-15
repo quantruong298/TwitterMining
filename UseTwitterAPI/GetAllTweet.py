5a9a108233798998c97cdc5d63770bd9e62f6f4f
quan <kubim298@gmail.com>
2019-12-15 21:52:28 +0700

    API Server: return topics

--- UseTwitterAPI/GetAllTweet.py
+++ UseTwitterAPI/GetAllTweet.py
@@ -125,7 +125,26 @@ def findTopics():
     # Do lemmatization keeping only noun, adj, vb, adv
     data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
 
-    return data_lemmatized
+    # Create Dictionary
+    id2word = corpora.Dictionary(data_lemmatized)
+
+    # Create Corpus
+    texts = data_lemmatized
+
+    # Term Document Frequency
+    corpus = [id2word.doc2bow(text) for text in texts]
+
+    # Build LDA model
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                                id2word=id2word,
+                                                num_topics=20,
+                                                random_state=100,
+                                                update_every=1,
+                                                chunksize=100,
+                                                passes=5,
+                                                alpha='auto',
+                                                per_word_topics=True)
+    return json.dumps(lda_model.print_topics())
 
 
 if __name__ == '__main__':
